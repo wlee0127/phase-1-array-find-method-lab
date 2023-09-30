61c475cdc856819a95107e3ddbeeb96e9d8f2440
william lee <will.lee.npc@gmail.com>
2023-09-30 03:39:45 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "N"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+];
+
+function isWin(object){
+    debugger;
+   return object.result === "W";
+}
+
+function superbowlWin(array){
+    debugger;
+    let answer = array.find(isWin);
+    if (answer != undefined){
+        return answer.year;
+    }else {
+        return answer;
+    }
+}
+
+//let answer = record.find(element => element.result === "W")
+//console.log(answer)
+
+console.log(superbowlWin(record))
+
